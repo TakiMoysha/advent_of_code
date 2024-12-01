f222e5669b7f2c96c9af171048492a21285a7d93
TakiMoysha <mpwema782@gmail.com>
2024-12-01 20:57:37 +0100

    chore: ref-ing

--- src/bin/1_historian_hysteria.rs
+++ src/bin/1_historian_hysteria.rs
@@ -1,6 +1,9 @@
+use std::collections::hash_map;
+
 use advent_of_code::read_csv;
 
-const DATASET_PATH: &str = "./data/1_historian_hysteria.csv";
+const FIRST_DATASET_PATH: &str = "./data/1_historian_hysteria.csv";
+const SECOND_DATASET_PATH: &str = "./data/1_historian_hysteria_2.csv";
 
 fn parse_line(line: &str) -> Vec<i32> {
     line.split_whitespace()
@@ -8,33 +11,56 @@ fn parse_line(line: &str) -> Vec<i32> {
         .collect::<Vec<i32>>()
 }
 
-#[derive(Debug, PartialEq)]
-struct CleanDataset(Vec<i32>, Vec<i32>);
-
-fn clean_dataset(dataset: Vec<String>) -> CleanDataset {
-    let mut a = Vec::with_capacity(dataset.len());
-    let mut b = Vec::with_capacity(dataset.len());
+fn clean_dataset(dataset: Vec<String>) -> (Vec<i32>, Vec<i32>) {
+    let mut left = Vec::with_capacity(dataset.len());
+    let mut right = Vec::with_capacity(dataset.len());
 
     for line in dataset.iter() {
         let p_line = parse_line(line);
-        a.push(p_line[0]);
-        b.push(p_line[1]);
+        left.push(p_line[0]);
+        right.push(p_line[1]);
     }
 
-    a.sort();
-    b.sort();
-    CleanDataset(a, b)
+    left.sort();
+    right.sort();
+
+    return (left, right);
 }
 
-fn find_smallest_distance(data: &CleanDataset) -> i32 {
-    return data.0.iter().zip(data.1.iter()).map(|(l, r)| (l - r).abs()).sum();
+fn find_smallest_distance(left: Vec<i32>, right: Vec<i32>) -> i32 {
+    return left
+        .iter()
+        .zip(right.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum();
+}
+
+// fn find_smallest_distance_with_similarity_score(left: Vec<i32>, right: Vec<i32>) -> (i32, i32) {
+//     let similarity_score = 0;
+//     let distance = left
+//         .iter()
+//         .zip(right.iter())
+//         .map(|(l, r)| (l - r).abs())
+//         .sum();
+//     (0, 0)
+// }
+
+fn part_1() {
+    let dataset = read_csv(FIRST_DATASET_PATH);
+    let (left, right) = clean_dataset(dataset);
+    let res = find_smallest_distance(left, right);
+    println!("Part 1: {res}");
+}
+
+fn part_2() {
+    let dataset = read_csv(SECOND_DATASET_PATH);
+    let (left, right) = clean_dataset(dataset);
+    // let res = find_smallest_distance_with_similarity_score(left, right);
+    // println!("Part 2: {res:?}");
 }
 
 fn main() {
-    let dataset = read_csv(DATASET_PATH);
-    let data = clean_dataset(dataset);
-    let res = find_smallest_distance(&data);
-    println!("{res}");
+    part_2();
 }
 
 #[cfg(test)]
@@ -53,8 +79,8 @@ mod tests {
     }
 
     #[rstest]
-    #[case(vec!["8 2", "3 4", "2 1"], CleanDataset(vec![2, 3, 8], vec![1, 2, 4]))]
-    fn test_cleaning_dataset(#[case] inputs: Vec<&str>, #[case] res: CleanDataset) {
+    #[case(vec!["8 2", "3 4", "2 1"], (vec![2, 3, 8], vec![1, 2, 4]) )]
+    fn test_cleaning_dataset(#[case] inputs: Vec<&str>, #[case] res: (Vec<i32>, Vec<i32>)) {
         let inputs = inputs.iter().map(|el| el.to_string()).collect();
         let parsed = clean_dataset(inputs);
         assert_eq!(parsed, res);
@@ -64,8 +90,23 @@ mod tests {
     #[case(vec!["3 4", "4 3", "2 5", "1 3", "3 9", "3 3"], 11)]
     fn tdd(#[case] inputs: Vec<&str>, #[case] res: i32) {
         let inputs = inputs.iter().map(|el| el.to_string()).collect();
-        let parsed = clean_dataset(inputs);
-        let my_res = find_smallest_distance(&parsed);
+        let (left, right) = clean_dataset(inputs);
+        let my_res = find_smallest_distance(left, right);
+        assert_eq!(my_res, res);
+    }
+
+    #[test]
+    fn test_part_one() {
+        part_1();
+    }
+
+    #[ignore = "not ready yet"]
+    #[rstest]
+    #[case(vec!["3 4", "4 3", "2 5", "1 3", "3 9", "3 3"], 31)]
+    fn test_part_two(#[case] inputs: Vec<&str>, #[case] res: i32) {
+        let inputs = inputs.iter().map(|el| el.to_string()).collect();
+        let (left, right) = clean_dataset(inputs);
+        let my_res = find_smallest_distance(left, right);
         assert_eq!(my_res, res);
     }
 }
