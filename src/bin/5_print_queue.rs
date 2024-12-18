188e04a71247673f8864d6946497afb0338df228
TakiMoysha <mpwema782@gmail.com>
2024-12-18 18:08:57 +0100

    stub for 5-2

--- src/bin/5_print_queue.rs
+++ src/bin/5_print_queue.rs
@@ -43,7 +43,7 @@ impl FromStr for UpdateSchema {
     }
 }
 
-fn check_produce_udpate_ordering(
+fn check_produce_update_schema(
     rules: &Vec<OrderingRule>,
     schema: &UpdateSchema,
 ) -> Result<(), String> {
@@ -52,7 +52,9 @@ fn check_produce_udpate_ordering(
 
         if !(schema.0.contains(&rule.0) && schema.0.contains(&rule.1)) {
             continue;
-        } else if schema.0.contains(&rule.0) {
+        }
+
+        if schema.0.contains(&rule.0) {
             let first = schema.0.iter().position(|x| *x == rule.0).unwrap_or(0);
             let second = schema.0.iter().position(|x| *x == rule.1).unwrap_or(1);
             if first < second {
@@ -67,16 +69,39 @@ fn check_produce_udpate_ordering(
     Ok(())
 }
 
+fn fix_produce_update_schema(rules: &Vec<OrderingRule>, schema: &UpdateSchema) -> UpdateSchema {
+    println!("DEBUG: Rules: {rules:?}; Produce: {schema:?}");
+    let mut _schema = schema.0.clone();
+    for rule in rules {
+        if !(_schema.contains(&rule.0) && _schema.contains(&rule.1)) {
+            continue;
+        } else if _schema.contains(&rule.0) {
+            let first = _schema.iter().position(|x| *x == rule.0).unwrap_or(0);
+            let second = _schema.iter().position(|x| *x == rule.1).unwrap_or(1);
+            if first > second {
+                // rule violation
+                todo!("i'm too lazy for implement this, but i know how to do it and will do it later");
+            }
+        }
+    }
+
+    todo!()
+}
+
+fn get_middle_number(up_schema: &UpdateSchema) -> u32 {
+    *up_schema.0.index(up_schema.0.len() / 2)
+}
+
 pub fn part_one(section_one: Vec<&str>, section_two: Vec<&str>) -> u32 {
     let s_one = parse_struct_from_str::<OrderingRule>(section_one).unwrap();
     let s_two = parse_struct_from_str::<UpdateSchema>(section_two).unwrap();
     let mut sum_of_middle = 0;
 
     for up_schema in &s_two {
-        let res = check_produce_udpate_ordering(&s_one, up_schema);
+        let res = check_produce_update_schema(&s_one, up_schema);
         if res.is_ok() {
-            let middle_number = up_schema.0.index(up_schema.0.len() / 2);
-            sum_of_middle += *middle_number;
+            let middle_number = get_middle_number(up_schema);
+            sum_of_middle += middle_number;
             println!("DEBUG: Success: {up_schema:?}, Middle: {middle_number:?}");
         } else {
             println!("DEBUG: Failed: {up_schema:?}, Error: {res:?}");
@@ -154,5 +179,44 @@ mod tests {
     ) {
         let sum_of_middle = part_one(section_one.to_vec(), section_two.to_vec());
         assert_eq!(sum_of_middle, res);
+    }
+
+    #[rstest]
+    #[case(DATA_RULE_ORDERING.to_vec(), DATA_PRODUCE.to_vec(), 0)]
+    fn test_part_two(
+        #[case] section_one: Vec<&str>,
+        #[case] section_two: Vec<&str>,
+        #[case] res: u32,
+    ) {
+        let s_one = parse_struct_from_str::<OrderingRule>(section_one).unwrap();
+        let s_two = parse_struct_from_str::<UpdateSchema>(section_two).unwrap();
+        let mut bad_schemas = Vec::new();
+
+        for up_schema in &s_two {
+            let res = check_produce_update_schema(&s_one, up_schema);
+            if res.is_err() {
+                println!("{:?}", res.err());
+                bad_schemas.push(up_schema);
+            }
+        }
+
+        let sum_of_middle_after_fix: u32 = bad_schemas
+            .iter()
+            .map(|b_schema| fix_produce_update_schema(&s_one, b_schema))
+            .map(|fix_schema| get_middle_number(&fix_schema))
+            .sum();
+
+        println!("result: {:#?}", sum_of_middle_after_fix);
+        // assert_eq!(sum_of_middle_after_fix, res);
+    }
+
+    #[rstest]
+    #[case(UpdateSchema(Vec::from([75, 97, 47, 61, 53])), UpdateSchema(Vec::from([97, 75, 47, 61, 53])))]
+    #[case(UpdateSchema(Vec::from([61, 13, 29])), UpdateSchema(Vec::from([61, 29, 47])))]
+    #[case(UpdateSchema(Vec::from([97, 13, 75, 29, 47])), UpdateSchema(Vec::from([97, 75, 47, 29, 13])))]
+    fn test_fix_produce_update_schema(#[case] schema: UpdateSchema, #[case] res: UpdateSchema) {
+        let rules = parse_struct_from_str::<OrderingRule>(DATA_RULE_ORDERING.to_vec()).unwrap();
+        let my_res = fix_produce_update_schema(&rules, &schema);
+        assert_eq!(my_res, res);
     }
 }
